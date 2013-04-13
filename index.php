596cbebca9bc148145c78f67303ee3d0a6a4974b
PeterTheOne <petertheone@gmail.com>
2013-04-13 17:13:55 +0200

    load data from file and print as map and list

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,maximum-scale=1.0,initial-scale=1.0,minimum-scale=1.0">
+    <title>Austria</title>
+    <link rel="stylesheet" href="stylesheets/stylesheet.css">
+    <script src="javascripts/modernizr.js"></script>
+  </head>
+<?php
+  include_once('mapfunctions.php');
+
+  $mapData = json_decode(file_get_contents('mapdata.json'));
+  $highestPartyIds = getHighestPercentagePartyId($mapData);
+?>
+  <body>
+
+  <div class="wrapper">
+    <?php printMap($highestPartyIds); ?>
+
+    <div class="text">
+      <p class="wahlwetter-header">Wahlwetter</p>
+      <?php printList($mapData); ?>
+    </div>
+  </div>
+
+  </body>
+</html>
